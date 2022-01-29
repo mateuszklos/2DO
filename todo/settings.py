2f6a810b435b6d985f382ba043722b654a86ed85
mklo <mateusz.klos@noblesoft.pl>
2022-01-29 23:14:26 +0100

    heroku commit

--- todo/settings.py
+++ todo/settings.py
@@ -137,5 +137,5 @@ SESSION_SAVE_EVERY_REQUEST = True  # will prevent from logging you out after 300
 
 import dj_database_url
 
-prod_db = dj_database_url.config(conn_max_age=500)
+prod_db = dj_database_url.config()
 DATABASES['default'].update(prod_db)
