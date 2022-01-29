4a683d109ab1365ce230a9bcad09f7b65836c474
mklo <mateusz.klos@noblesoft.pl>
2022-01-30 00:35:22 +0100

    heroku planner commit

--- todo/settings.py
+++ todo/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'd7s16vlhdbso6s',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'kuiqsfnrirdzoz',
